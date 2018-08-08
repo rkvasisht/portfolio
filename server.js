4b66f838cf5a19ced75427e8a6d4e3e332227818
rkvasisht <rkvasisht@gmail.com>
2018-08-08 09:48:54 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,42 @@
+
+require('dotenv').config();
+const express = require('express');
+const bp = require('body-parser');
+const ejsLayouts = require('express-ejs-layouts');
+const port = process.env.PORT || 3000;
+const fs = require('fs');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/static'));
+app.use(bp.urlencoded({extended: true}));
+app.use(ejsLayouts);
+
+app.get('/', (req, res) => {
+	
+	res.render('index');
+});
+
+
+app.get('/projects', (req, res) => {
+	
+	res.render('projects');
+});
+
+app.get('/contact', (req, res) => {
+	
+	res.render('contact');
+});
+
+app.get('/about', (req, res) => {
+	
+	res.render('about');
+});
+
+app.get('/resume', (req, res) => {
+	
+	res.render('resume');
+});
+
+app.listen(port, function() {console.log('Hooked on ' + port)});
+
---
+++ static/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+
+    // Initialize Materialize components
+    M.AutoInit();
+    $('.parallax').parallax();
+})
